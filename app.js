6969c1e40fc54c1609b41676120507912a41e75c
Lukas <lukas.klier@oscillate.ch>
2021-02-14 19:09:52 +0100

    remove babel

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 const app = express();
 const port = 3000;
 
@@ -7,7 +7,7 @@ require("dotenv").config({ path: __dirname + "/.env" });
 const { serverStartup } = require("./src/server.js");
 
 app.get("/", async (req, res) => {
-  res.send('This is the Jollity Bot');
+  res.send("This is the Jollity Bot");
 });
 
 app.listen(port, () => {
--- src/chatId/getChatIds.js
+++ src/chatId/getChatIds.js
@@ -1,25 +1,27 @@
-import S3 from "aws-sdk/clients/s3";
+const S3 = require("aws-sdk/clients/s3");
 
-export const getChatIds = () =>
-  new Promise((resolve, reject) => {
-    const s3 = new S3();
+module.exports = {
+  getChatIds: () =>
+    new Promise((resolve, reject) => {
+      const s3 = new S3();
 
-    const getParams = {
-      Bucket: "jollity-track",
-      Key: "chatIds.json",
-    };
+      const getParams = {
+        Bucket: "jollity-track",
+        Key: "chatIds.json",
+      };
 
-    s3.getObject(getParams, (err, data) => {
-      if (err) {
-        console.log("err", err);
-        reject(err);
-        return;
-      } else {
-        const JSONString = data.Body.toString();
-        const chatIdsJSON = JSON.parse(JSONString);
-        resolve(chatIdsJSON.chatIds);
-      }
-    });
+      s3.getObject(getParams, (err, data) => {
+        if (err) {
+          console.log("err", err);
+          reject(err);
+          return;
+        } else {
+          const JSONString = data.Body.toString();
+          const chatIdsJSON = JSON.parse(JSONString);
+          resolve(chatIdsJSON.chatIds);
+        }
+      });
 
-    return;
-  });
+      return;
+    }),
+};
