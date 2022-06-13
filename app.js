e30e73fce1a8fcdbebba59b38dec6def13a516e4
Pascal Keel <pascal.keel@MacBook-Pro-3.fritz.box>
2022-06-13 12:22:53 +0200

    Remove cronjob. Add to base call a onetime request

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const port = 8080;
 console.log("Starting APPPPPPPPP");
 
 require("dotenv").config({ path: __dirname + "../.env" });
+const { updateIridiumLocation } = require("./src/telegram/getIridiumLocation.js");
 
-const { serverStartup } = require("./src/server.js");
+// const { serverStartup } = require("./src/server.js");
 
 app.get("/", async (req, res) => {
+  updateIridiumLocation();
   res.status(200).send("This is the Passage Bot").end();
   // res.status(200).send('Hello, world!').end();
 });
 
-app.listen(port, () => {
-  console.log("Starting Server: ", process.env.IMAP_USER);
-  serverStartup();
-});
+// app.listen(port, () => {
+//   console.log("Starting Server: ", process.env.IMAP_USER);
+//   serverStartup();
+// });
