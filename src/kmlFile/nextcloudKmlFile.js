046ef372501c338c137ba2b8e466dc07bfd842c8
Pascal Keel <pascal.keel@MacBook-Pro-3.fritz.box>
2022-06-13 10:43:38 +0200

    Format code, try to fix googles unexpoected token

--- src/kmlFile/nextcloudKmlFile.js
+++ src/kmlFile/nextcloudKmlFile.js
@@ -3,7 +3,7 @@ const { default: Client } = require("nextcloud-node-client");
 const tokml = require("tokml");
 const DOMParser = require("xmldom").DOMParser;
 require("nextcloud-node-client");
-const { getLon, getLat, isValid } =require("../imap/emailparser.js")
+const { getLon, getLat, isValid } = require("../imap/emailparser.js")
 
 /* const getLon = (string) => {
   const regex = /Lon([+-]+.[\S]+)/;
@@ -33,63 +33,63 @@ async function nextcloudKmlFile(messages) {
     const kmlfile = await folder.getFile(kmlFilename);
     const jsonfile = await folder.getFile(jsonFilename);
     console.log("kmlfile", kmlfile);
-    
-    const data = await kmlfile?.getContent() ?? "";
 
-        console.log("your file", data);
-        const oldKmlString = data.toString('utf-8');
-        console.log("your file to string", data.toString('utf-8'));
-        const oldKmlDom = new DOMParser().parseFromString(
-          oldKmlString,
-          "text/xml"
-        );
+    const data = (await kmlfile.getContent()) || "";
 
-        var geoJSON = togeojson.kml(oldKmlDom);
+    console.log("your file", data);
+    const oldKmlString = data.toString('utf-8');
+    console.log("your file to string", data.toString('utf-8'));
+    const oldKmlDom = new DOMParser().parseFromString(
+        oldKmlString,
+        "text/xml"
+    );
 
-        const newCoordinates = messages
-          .filter((message) => isValid(message.text))
-          .map((message) => {
-              return ([getLon(message.text), getLat(message.text)])
-          });
-        console.log(
-          "🚀 ~ file: updateKmlFile.js ~ line 95 ~ s3.getObject ~ geoJSON.features[0].geometry",
-          geoJSON.features[0].geometry
-        );
+    var geoJSON = togeojson.kml(oldKmlDom);
 
-        // cgitonsole.log("inValidMessages:", messages.filter((message) => !isValid(message.text)));
+    const newCoordinates = messages
+        .filter((message) => isValid(message.text))
+        .map((message) => {
+            return ([getLon(message.text), getLat(message.text)])
+        });
+    console.log(
+        "🚀 ~ file: updateKmlFile.js ~ line 95 ~ s3.getObject ~ geoJSON.features[0].geometry",
+        geoJSON.features[0].geometry
+    );
 
-        geoJSON.features[0].geometry = {
-          ...geoJSON.features[0].geometry,
-          coordinates: geoJSON.features[0].geometry.coordinates.concat(
+    // cgitonsole.log("inValidMessages:", messages.filter((message) => !isValid(message.text)));
+
+    geoJSON.features[0].geometry = {
+        ...geoJSON.features[0].geometry,
+        coordinates: geoJSON.features[0].geometry.coordinates.concat(
             newCoordinates
-          ),
-        };
-        console.log(
-          "🚀 ~ file: updateKmlFile.js ~ line 95 ~ s3.getObject ~ geoJSON.features[0].geometry",
-          geoJSON.features[0].geometry
-        );
+        ),
+    };
+    console.log(
+        "🚀 ~ file: updateKmlFile.js ~ line 95 ~ s3.getObject ~ geoJSON.features[0].geometry",
+        geoJSON.features[0].geometry
+    );
 
-        // generate KML
-        const kmlData = tokml(geoJSON);
+    // generate KML
+    const kmlData = tokml(geoJSON);
 
-        console.log(
-          "🚀 ~ file: updateKmlFile.js ~ line 41 ~ updateKmlFile: ~ kmlData",
-          kmlData
-        );
+    console.log(
+        "🚀 ~ file: updateKmlFile.js ~ line 41 ~ updateKmlFile: ~ kmlData",
+        kmlData
+    );
 
-        const currentDate = new Date();
-        const expireDate = new Date(currentDate.getTime() + 5 * 60000);
-        console.log(
-          "🚀 ~ file: updateKmlFile.js ~ line 89 ~ s3.getObject ~ expireDate",
-          currentDate,
-          expireDate
-        );
+    const currentDate = new Date();
+    const expireDate = new Date(currentDate.getTime() + 5 * 60000);
+    console.log(
+        "🚀 ~ file: updateKmlFile.js ~ line 89 ~ s3.getObject ~ expireDate",
+        currentDate,
+        expireDate
+    );
 
-        await kmlfile.delete();
-        folder.createFile(kmlFilename, Buffer.from(kmlData));
+    await kmlfile.delete();
+    folder.createFile(kmlFilename, Buffer.from(kmlData));
 
-        await jsonfile.delete();
-        folder.createFile(jsonFilename, Buffer.from(JSON.stringify(geoJSON, null, 2)));
+    await jsonfile.delete();
+    folder.createFile(jsonFilename, Buffer.from(JSON.stringify(geoJSON, null, 2)));
 
     return;
 }
